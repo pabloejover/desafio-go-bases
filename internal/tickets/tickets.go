a922e88d28c90a1ed73958d0c261e2bc79e31260
pabloejover <pablo.e.jover@gmail.com>
2023-08-25 03:38:22 -0300

    Lectura de archivo csv

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -1,13 +1,32 @@
 package tickets
 
+import "fmt"
+
+// Ticket es una estructura que define un ticket de una reserva
 type Ticket struct {
+	Id      string
+	Nombre  string
+	Email   string
+	Destino string
+	Hora    string
+	Precio  string
+}
+
+// Listado de tickets
+type Storage struct {
+	Tickets []Ticket
+}
+
+func (s *Storage) PrintInfo() {
+	fmt.Printf("%v+", s.Tickets)
+
 }
 
 // ejemplo 1
-func GetTotalTickets(destination string) (int, error) {}
+//func GetTotalTickets(destination string) (int, error) {}
 
 // ejemplo 2
-func GetMornings(time string) (int, error) {}
+//func GetMornings(time string) (int, error) {}
 
 // ejemplo 3
-func AverageDestination(destination string, total int) (int, error) {}
+//func AverageDestination(destination string, total int) (int, error) {}
--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import ()
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/pabloejover/desafio-go-bases.git/internal/tickets"
+)
+
+const (
+	filename = "./tickets.csv"
+)
 
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	storage := tickets.Storage{
+		Tickets: readFile(filename),
+	}
+
+	storage.PrintInfo()
+
+}
+
+// readFile es una funcion creada para leer un archivo
+func readFile(filename string) []tickets.Ticket {
+	file, err := os.ReadFile(filename)
+
+	if err != nil {
+		panic(err)
+	}
+	data := strings.Split(string(file), "\n")
+
+	var resultado []tickets.Ticket
+
+	for i := 0; i < len(data); i++ {
+		if len(data[i]) > 0 {
+			file := strings.Split(string(data[i]), ",")
+			ticket := tickets.Ticket{
+				Id:      file[0],
+				Nombre:  file[1],
+				Email:   file[2],
+				Destino: file[3],
+				Hora:    file[4],
+				Precio:  file[5],
+			}
+			resultado = append(resultado, ticket)
+		}
+
+	}
+	return resultado
 
 }
